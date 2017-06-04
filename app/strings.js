8c99f44a6d3bcf0256aabd50ccace40137e17523
druchefavour <druchefavour@gmail.com>
2017-06-03 21:50:17 -0500

    commit answers update2

--- app/strings.js
+++ app/strings.js
@@ -1,5 +1,4 @@
 exports = typeof window === 'undefined' ? global : window;
-
 exports.stringsAnswers = {
 	reduceString: function(str, amount) {
 		// Declare all the required variables: count and result
@@ -7,66 +6,64 @@ exports.stringsAnswers = {
 		var result = ''; // An empty String
 		var strArray = []; // An empty Array
 		var string = str.split(''); //Split str into an array of substrings
-
-		// loop across the and count all the strings
-		//for (var i = 0; i < str.length; i++) {
-		//	if (str[i] === str[i+1]) {
-
-		//		count ++;
-		//		} else if (count < amount) {
-		//			result += str[i];
-		//		}
-
-		//		} else {
-
-		//			count = 0;
-		//			result += str[i];
-		//		}
-		//	};
-			return strArray.join(''); //Used to join the elements of an array into a newStr
+		string.forEach(function(val) {
+			if (val != result) {
+				result = val;
+				count = 1;
+				strArray.push(val);
+			} else if (count < amount) {
+				count++;
+				strArray.push(val);
+			}
+		});
+		return strArray.join(''); //Used the join method to join the elements of an array into a newStr
      },
-	
-function wordWrap (str, cols) {
-  	// Declare a variable that wraps the string and assign an empty string to it
-  	var wrappedString = '', 
-  	// Define an empty array
-  	arrayOfWords = [];
 
-  	//Use the split method to define each string element
+      wordWrap: function (str, cols) {
+      	var string = str.split('');
+      	var i = cols;
 
-  	arrayOfWords = str.split('');
+      	function checkMore() {
+      		for (i; i < string.length; i++) {
+      			if (string[i] === ' ') {
+      				string[i] = '\n';
+      				i += cols;
+      				 return;
+      			}
+      		}
+      	}
+      	function checkLess() {
+      		for (i; i > 0; i--) {
+      			if (string[i] === ' ') {
+      				string[i] = '\n';
+      				i += cols;
+      				return;
+      			}
+      		}
 
-  	// Assign the first word in the array to wrappedString
+      		checkMore();
 
-  	wrappedString = arrayOfWords [0]; 
-  	// loop over the array of  words and select the wrapped words
+      	}
+      	while (i < string.length) {
+      		if (string.indexOf(' ') < 0) {
+      			 return string.join('')
+      		}
+      		 checkLess();
+      	}
 
-  	for (var i = 1; i < arrayOfWords.length; i++) {
-  		if (arrayOfWords[i].length > cols) {
-	        wordWrap += '\n' + arrayOfWords[i];
-	    } else {
+      	return string.join('')
 
-	    	if (wordWrap.length + arrayOfWords[i].length > cols) {
-	    		wordWrap += '\n' +arrayOfWords[I];
-	    		} else {
-	    			wordWrap += ' ' + arrayOfWords[i];
-	    		}
-	    }
-}  
+      },
 
-return (wrappedString);
 
-  },
-
-reverseString: function (str) {
-//Declare a string as a variable
-var splitString = '';
-for (var i = str.length -1; i >= 0; i--) {
-
-	splitString += str[i];
-}
-return splitString; 
-}
+     reverseString: function (str) {
+     //Declare a string as a variable
+     var splitString = '';
+     for (var i = str.length -1; i >= 0; i--) {
+     	splitString += str[i];
+     }
+      return splitString; 
+ }
 
 };
 
